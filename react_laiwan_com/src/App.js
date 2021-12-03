def21e9d3c6b716f078ee79284b78285c1aaea74
Anand <33146742+anandzhang@users.noreply.github.com>
2021-12-03 12:25:10 +0800

    无效链接重定向到主页 (#203)

--- react_laiwan_com/src/App.js
+++ react_laiwan_com/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import {
+    HashRouter as Router,
+    Route,
+    Redirect,
+    Switch,
+} from 'react-router-dom';
 import H5Tutorial from './page/h5-tutorial/controller/H5Tutorial';
 import HomeScreen from './page/home/controller/HomeScreen';
 import Tutorial from './Tutorial';
 
 const App = () => (
     <Router>
-        <Route path="/" component={HomeScreen} exact />
-        <Route path="/h5-tutorial" component={H5Tutorial} />
-        <Route path="/tutorial" component={Tutorial} />
+        <Switch>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/h5-tutorial" component={H5Tutorial} />
+            <Route path="/tutorial" component={Tutorial} />
+            <Redirect to="/" />
+        </Switch>
     </Router>
 );
 
