8766ee4d3defc1be4eb263ef88d9b86d91db7525
Luo-Lotus <98867591+Luo-Lotus@users.noreply.github.com>
2022-11-03 10:33:50 +0800

    修复生成的二维码无法扫描 (#221)

--- react_laiwan_com/src/view/Qrcode.js
+++ react_laiwan_com/src/view/Qrcode.js
@@ -8,17 +8,18 @@ import icon from '../source/icon.png';
 export default class Qrcode extends Component {
     render() {
         const { downloadUrl } = this.props
+
         return (
             <QRCodeSVG
                 value={downloadUrl}
                 size={120}
-
-                style={{border: '#fff solid 4px'}}
+                style={{ border: '#fff solid 4px' }}
+                level="M"
                 imageSettings={{
                     src: icon,
                     excavate: true,
-                    height: 25,
-                    width: 25,
+                    height: 30,
+                    width: 30,
                 }}
             />
         );
